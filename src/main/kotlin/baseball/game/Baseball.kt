66d2549c07cfe87f98efc6c9a6b86aec68a98ed7
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-01 22:18:27 +0900

    [Feat] (Baseball.kt) 스트라이크 3개일때 입력하는 기능 추가

--- src/main/kotlin/baseball/game/Baseball.kt
+++ src/main/kotlin/baseball/game/Baseball.kt
@@ -10,6 +10,9 @@ class Baseball(
     private val computer: Computer,
     private val answerBoard: AnswerBoard
 ) : Game {
+    private var gameState = 1
+    private var answer = computer.createAnswer()
+
     override fun play() {
         println("숫자 야구 게임을 시작합니다.")
         process()
@@ -18,18 +21,17 @@ class Baseball(
     // 게임 진행
     override fun process() {
         do {
-            /*
-            * 사용자 입력을 받는다.
-            * 정답과 사용자 입력을 받아서 결과를 출력한다
-            * 정답을 맞추면 종료, 재시작을 입력 받는다.
-            * */
+            /* 정답과 사용자 입력을 받아서 결과를 출력한다*/
+            val userNumber = user.createNumber()
+
             finish()
-        } while (/* 종료 코드를 받을 때 까지 반복*/ TODO())
+        } while (isPlaying())
     }
 
-    // 게임 종료 코드 실행
+    private fun isPlaying(): Boolean = gameState != 2
+
     override fun quit() {
-        TODO("Not yet implemented")
+        gameState = 2
     }
 
     // 제거하고 다시 시작
@@ -39,10 +41,18 @@ class Baseball(
 
     // 3개 다 맞췄을 때 끝
     private fun finish() {
-        /*스트라이크가 3일 때
-        종료 메시지를 출력한다.
-        * 사용자 입력을 받고
-        * 재시작 1 = retry, 종료 2 = quit*/
+        /*스트라이크가 3일 때*/
+
+        if (/*스트라이크가 3일 때*/TODO()) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+            // TODO: 1, 2이외의 숫자 예외처리
+            when (readLine()) {
+                "1" -> retry()
+                "2" -> quit()
+            }
+        }
     }
 
 }
