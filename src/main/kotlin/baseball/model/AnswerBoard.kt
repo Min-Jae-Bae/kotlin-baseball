cf75de5efe7e6e9d17a14ef2de3cbb82ad4d4f5c
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-01 23:07:41 +0900

    [Feat] (AnswerBoard.kt) 입력 숫자에 대한 결과 출력 기능

---
+++ src/main/kotlin/baseball/model/AnswerBoard.kt
@@ -0,0 +1,56 @@
+package baseball.model
+
+enum class BaseballState {
+    STRIKE, BALL, OUT
+}
+
+class AnswerBoard {
+    // 상태를 모두 아웃으로 초기화
+    private val stateList = MutableList(3) { BaseballState.OUT }
+
+
+    private fun createCount(): Triple<Int, Int, Int> {
+        var strikeCount = 0
+        var ballCount = 0
+        var outCount = 0
+
+        stateList.forEach { state ->
+            when (state) {
+                BaseballState.STRIKE -> strikeCount++
+                BaseballState.BALL -> ballCount++
+                BaseballState.OUT -> outCount++
+            }
+        }
+        return Triple(strikeCount, ballCount, outCount)
+    }
+
+    fun createResult(answer: String, userNumber: String) {
+        answer.forEachIndexed { answerIndex, answerNumber ->
+            if (answerNumber == userNumber[answerIndex]) {
+                stateList.add(BaseballState.STRIKE)
+            }
+            if (answerNumber != userNumber[answerIndex] && answer.contains(userNumber[answerIndex])) {
+                stateList.add(BaseballState.BALL)
+            }
+            if (answerNumber != userNumber[answerIndex] && !answer.contains(userNumber[answerIndex])) {
+                stateList.add(BaseballState.OUT)
+            }
+        }
+    }
+
+    fun printResult(): String {
+        val (strikeCount, ballCount, outCount) = createCount()
+        var message = ""
+
+        // 아웃일 때 출력
+        if (outCount == stateList.size) message = "낫싱"
+        // 스트라이크일때 출력
+        if (ballCount == 0 && strikeCount != 0) message = "%d스트라이크"
+        // 볼일때 출력
+        if (ballCount != 0 && strikeCount == 0) message = "%d볼"
+        // 스트라이크 볼일 때
+        if (ballCount != 0 && strikeCount != 0) message = "%d볼 %d스트라이크"
+
+        return message
+    }
+}
